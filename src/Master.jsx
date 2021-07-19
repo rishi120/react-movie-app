872eed4fe84ccab5c93f3ff61e3262b856c81672
Chandan <chandan.b@pacewisdom>
2021-07-19 18:57:36 +0530

    fixed data loading issue

--- src/Master.jsx
+++ src/Master.jsx
@@ -42,6 +42,7 @@ const Rendermastercomponent = () => {
       .catch((error) => {
         setShowError(true);
         setShowMovieIcon(false);
+        setMovieError(false);
       });
   }
   const handleClose = () => {
